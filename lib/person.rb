58c3579c0c9e3e7bd42b2e8e1addaca11fd04ba7
derrick b <dbozich1@gmail.com>
2018-06-03 09:05:34 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    instance_of_person = Person.new
+    instance_of_person = Person.new(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 
